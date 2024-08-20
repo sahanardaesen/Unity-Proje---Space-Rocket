4103e09523dae29c6575253b8a75bdd70962072e
sahanardaesen <sahanardaesen@outlook.com>
2024-08-20 14:00:03 +0300

    WallController Script Added
    With SceneManager restart the scene when collision occur.

---
+++ Space Rocket/Assets/GameFolders/Scripts/Concretes/Controllers/WallController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SpaceRocket.Controllers
+{
+    public class WallController : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision other) {
+            PlayerController player = other.collider.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+}
