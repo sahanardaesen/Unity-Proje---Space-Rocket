514236327250c2569a423b844bbafac671898171
sahanardaesen <sahanardaesen@outlook.com>
2024-08-22 15:18:12 +0300

    GameOver Canvas Updated
    GameOverPanel activated after game over.

---
+++ Space Rocket/Assets/GameFolders/Scripts/Concretes/UI/GameOverObject.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using SpaceRocket.Managers;
+using UnityEngine;
+
+namespace SpaceRocket.UI
+{
+    public class GameOverObject : MonoBehaviour
+    {
+        [SerializeField] GameObject _gameOverPanel;
+
+        private void Awake() {
+            if(_gameOverPanel.activeSelf) {
+                _gameOverPanel.SetActive(false);
+            }
+        }
+
+        private void OnEnable() {
+            GameManager.Instance.OnGameOver += OnGameOverHandler;
+        }
+
+        private void OnDisable() {
+            GameManager.Instance.OnGameOver -= OnGameOverHandler;
+        }
+
+        private void OnGameOverHandler()
+        {
+            Debug.Log("GameOver");
+            if(!_gameOverPanel.activeSelf) {
+                _gameOverPanel.SetActive(true);
+            }
+        }
+    }
+}
